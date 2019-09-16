ea1f9df2ed02c01254c3cdad37627c64838aeb1b
FR4NKENSTEIN <jacobleesmithy@gmail.com>
2019-09-16 16:26:50 -0700

    all CRUDS successful. Totally functional app.

--- server/controller.js
+++ server/controller.js
@@ -8,32 +8,45 @@ module.exports = {
             Response.render('index', {ravens: docs})
         );
     },
-    adoption: function(Request,Response){// should display a form
+    adopter: function(Request,Response){// should display a form
         console.log("######## @ ADOPT");
         Response.render('adopt');
     },
     displayer: function(Request,Response){// should display data
         console.log("######## @ DISPLAY");
-        Raven.findOne(Request.params)
+        Raven.findOne(Request.params)// using different methods from Mongoose docs is REALLLY important
             .then(oneRaven => Response.render('display', {aRaven: oneRaven})
             .catch(err => Response.json(err))
         );
     },
-    modify: function(Request,Response){// should display a form
+    modifier: function(Request,Response){// should display a form
         console.log("######## @ MODIFY");
-        Response.render('modify');
+        Raven.findOne(Request.params)
+            .then(oneRaven => Response.render('modify', {aRaven: oneRaven})
+            .catch(err => Response.json(err))
+        );
     },
     // PROCESSORS
-    adopter: function(Request,Response){
+    adoption: function(Request,Response){
         console.log("######## @ ADOPTION PROCESS");
         console.log('######## DATA: ', Request.body);
-        // Response.redirect('/raven/new');
-        const adoptee = new Raven();
-        adoptee.name = Request.body.name_input;
-        adoptee.age = Request.body.age_input;
-        adoptee.save().then(newRaven => {
-            console.log('######## NEW RAVEN: ', newRaven);
-            Response.redirect(`/raven/${newRaven._id}`);
-        });
+        let adoptee = Request.body;
+        Raven.create(adoptee)//make sure the names for your input's match the
+            .then(newRaven => {// the model field names EXACTLY!
+                console.log('######## NEW RAVEN: ', newRaven);
+                Response.redirect(`/raven/${newRaven._id}`)
+            });
+    },
+    modification: function(Request,Response){
+        Raven.updateOne(Request.params,{name: Request.body.name})
+            .then(Response.redirect('/'))//I can successfully update
+    },
+    destroyer: function(Request,Response){
+        console.log("######## @ DESTROYER");
+        console.log(Request.params);
+        let target = Request.params;
+        Raven.deleteOne(target)
+            .then(Response.redirect('/'))
+            .catch(err => Response.json(err));
     },
 }
--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,11 @@ const controller = require("./controller");
 // routes with the least to them (like a root/index route)
 // should be last/@ the bottom
 module.exports = function(app) {
-    app.post('/raven/destoy/:_id',)
-    app.get('/raven/edit/:_id', controller.modify)
-    app.get('/raven/new', controller.adoption)
+    app.get('/raven/destroy/:_id', controller.destroyer)
+    app.get('/raven/edit/:_id', controller.modifier)//related to .post/raven/:_id
+    app.get('/raven/new', controller.adopter)//related to .post/raven
     app.get('/raven/:_id', controller.displayer)
-    app.post('/raven', controller.adopter)
+    app.post('/raven/:_id', controller.modification)//related to .get/raven/edit/:_id
+    app.post('/raven', controller.adoption)//related to .get/raven/new
     app.get('/', controller.index)
 };
